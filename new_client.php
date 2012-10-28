6a9200894752eb6c1f6796da42787d075932b3f8
rmoutard <raphael.moutard@gmail.com>
2012-10-28 22:51:18 +0100

    index.php - to store in the database

--- new_client.php
+++ new_client.php
@@ -1,21 +1,24 @@
 <?php
-$username = "cottontracks";
-$password = "ctdb55qC";
-$hostname = "mysql51-38.perso"; 
+//$username = "cottontracks";
+//$password = "ctdb55qC";
+//$hostname = "mysql51-38.perso";
+$username = "root";
+$password = "";
+$hostname = "127.0.0.1:8889"; 
 
-$con = mysql_connect($username, $password, $hostname);
+$con = mysql_connect("127.0.0.1:8889", "root", "root");
 if (!$con) {
   die('Could not connect: ' . mysql_error());
 }
 
 // some code
 $email = $_POST['email'];
-if($email){		
+if($email){
   if(filter_var($email, FILTER_VALIDATE_EMAIL)){ 
   	//L'email est bonne
   	mysql_select_db("cottontracks", $con);
 
-  	$sql="INSERT INTO Clients (email)
+  	$sql="INSERT INTO clients (email)
 		VALUES
 		('$email')";
 
