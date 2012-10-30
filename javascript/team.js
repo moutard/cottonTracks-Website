0acce318c81964b563707f0f527febb53d514112
Raphael Korach <raphael.korach@gmail.com>
2012-10-30 15:57:50 +0000

    footer appears when positioned

--- javascript/team.js
+++ javascript/team.js
@@ -4,11 +4,16 @@ $(document).on("ready",function(){
 	});
 });
 
+var lancement = 0;
+
 function positionFooter() {
 	if ($('#header').height()+$('#main').height()+$('#footer').height() <= $(window).height()) {
 		$('#footer').css({'position': 'fixed'});
 	} else {
 		$('#footer').css({'position': 'static'});
+	}
+	if (lancement === 0) {
+		$('#footer').css({'opacity': '1'});
 	}
 }
 
