c6da5f130b9997ee5b81d6358586d7acc4eabd0c
Julihan <julien.deveaux@gmail.com>
2013-05-29 14:17:32 -0400

    Fixe resize window bug on triangles

--- triangle.js
+++ triangle.js
@@ -14,16 +14,28 @@ jQuery(document).ready(function($){
 	var triangle_number = (triangle_width/triangle_size)*line_number;
 	
 	//Create the triangles
-	for(i=0; i<triangle_number; i++){
-	  $("<div>")
-    .clone(false)
-    .appendTo("#triangle")
-    .addClass("triangle-right");
-    $("<div>")
-    .clone(false)
-    .appendTo("#triangle")
-    .addClass("triangle-left")
+	function creationTriangles(triangle_count){
+		for(i=0; i<triangle_count; i++){
+		  $("<div>")
+	    .clone(false)
+	    .appendTo("#triangle")
+	    .addClass("triangle-right");
+	    $("<div>")
+	    .clone(false)
+	    .appendTo("#triangle")
+	    .addClass("triangle-left")
+		};
 	};
+	
+	//Remove triangle nodes
+	function removeTriangles(triangle_count){
+		for(i=0; i<(triangle_count); i++){  
+			$(triangle_right[i]).remove();
+			$(triangle_left[i]).remove();
+		};
+	};
+	
+	creationTriangles(triangle_number);
 	
 	var triangle_right = $('#triangle .triangle-right');
 	var triangle_left = $('#triangle .triangle-left');
@@ -92,50 +104,80 @@ jQuery(document).ready(function($){
 	
 	//Turn triangle to light gray on click
 	$("#triangle").click(function(e){
-		clearTriangle();
+		initAnimationTriangle();
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_right.length * Math.random());
 	  setTriangleRightColor(pick, "0b9b96");
-		//$(triangle_right[pick]).toggleClass("triangle-right-0b9b96");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_left.length * Math.random());
-	  $(triangle_left[pick]).toggleClass("triangle-left-0b9b96");
+	  setTriangleLeftColor(pick, "0b9b96");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_right.length * Math.random());
 	  setTriangleRightColor(pick, "0f8692");
-		//$(triangle_right[pick]).toggleClass("triangle-right-0f8692");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_left.length * Math.random());
-	  $(triangle_left[pick]).toggleClass("triangle-left-0f8692");
+	  setTriangleLeftColor(pick, "0f8692");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_right.length * Math.random());
 	  setTriangleRightColor(pick, "13688c");
-		//$(triangle_right[pick]).toggleClass("triangle-right-13688c");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_left.length * Math.random());
-	  $(triangle_left[pick]).toggleClass("triangle-left-13688c");
+		setTriangleLefttColor(pick, "13688c");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_right.length * Math.random());
 	  setTriangleRightColor(pick, "0fab9a");
-		//$(triangle_right[pick]).toggleClass("triangle-right-0fab9a");
 	});
 	
 	$("#triangle").mousemove(function(e){
 	  pick = Math.round(triangle_left.length * Math.random());
-	  $(triangle_left[pick]).toggleClass("triangle-left-0fab9a");
+		setTriangleLeftColor(pick, "0fab9a");
+	});
+	
+	$("#triangle").mousemove(function(e){
+		console.log(triangle_number);
+	});
+	
+	$(window).resize(function() {
+		//Recalculate values at the creation
+		window_width = $(window).width();
+		triangle_width = (Math.floor(window_width/(2*triangle_size))+1)*2*triangle_size+triangle_size;
+		triangle_margin = -2*triangle_size*(1-window_width/(4*triangle_size)+Math.floor(window_width/(4*triangle_size)));
+		
+		//Update CSS
+		$("#triangle").width(triangle_width);
+		$("#triangle").css("margin-left", triangle_margin);
+		
+		var newTriangle_number = (triangle_width/triangle_size)*line_number;
+		//triangle_number = (triangle_width/triangle_size)*line_number;
+		
+		if (newTriangle_number-triangle_number>0){
+			creationTriangles(newTriangle_number-triangle_number);
+			triangle_number = newTriangle_number;
+			triangle_right = $('#triangle .triangle-right');
+			triangle_left = $('#triangle .triangle-left');
+			initAnimationTriangle();
+		}
+		else if (newTriangle_number-triangle_number<0){ //to make sure it's !=0. Otherwise it useless to run the next part of the script
+			console.log("TEST");
+			removeTriangles(triangle_number-newTriangle_number);
+			triangle_number = newTriangle_number;
+			triangle_right = $('#triangle .triangle-right');
+			triangle_left = $('#triangle .triangle-left');
+			initAnimationTriangle();
+		}
 	});
 });
