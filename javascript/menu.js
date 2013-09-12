5bd56ab3ec2b386afa39768f6148d2c497c46d9b
Julihan <julien.deveaux@gmail.com>
2013-09-13 01:15:15 +0200

    New Website Version

--- javascript/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function($){
-	$("#top_menu ul li a").mouseover(function(e){
-		$(this).find("span").toggleClass("underline");
-	}).mouseout(function(){
-		$(this).find("span").toggleClass("underline");
-	});
-});
---
+++ javascript/resize.js
@@ -0,0 +1,27 @@
+jQuery(document).ready(function($){
+		var window_height = $(window).height();
+		var window_width = $(window).width();
+
+		//Set CSS
+		if(window_width > 1060){
+			$("#main-page").css("margin-top", 1060);
+		}
+		else if(window_width > 899){
+			$("#main-page").css("margin-top", window_height);
+		}
+		//Window resizing
+		$(window).resize(function() {
+			window_height = $(window).height();
+			window_width = $(window).width();
+			
+			if(window_width > 1060){
+				$("#main-page").css("margin-top", 1060);
+			}
+			else if(window_width > 899){
+				$("#main-page").css("margin-top", window_height);
+			}
+			else{
+				$("#main-page").css("margin-top", 440);
+			}
+		});
+});
