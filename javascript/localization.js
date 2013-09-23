43df9bb4811645130b4082c40c67e85a2ecbd661
Julihan <julien.deveaux@gmail.com>
2013-09-23 23:58:51 +0200

    Adding localization script + es/ en/ htmls + fixing few typos

---
+++ javascript/localization.js
@@ -0,0 +1,7 @@
+var user_lang = window.navigator.userLanguage || window.navigator.language;
+if(user_lang == 'fr'){
+	window.location.href = "es/index.html";	
+}
+// else if(user_lang == 'fr'){
+// 	window.location.href = "fr/index.html";	
+// }
