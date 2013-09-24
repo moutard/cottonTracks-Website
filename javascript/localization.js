1afc1e9c06560368ee2cc262e55b991f4d0c7164
Julihan <julien.deveaux@gmail.com>
2013-09-24 18:55:05 +0200

    fix translation mistake in es/ + adding the script for fr, ru and ja

--- javascript/localization.js
+++ javascript/localization.js
@@ -1,7 +1,13 @@
 var user_lang = window.navigator.userLanguage || window.navigator.language;
-if(user_lang == 'fr' || 'fr-fr'){
+if(user_lang.match(/^es/)){ //match all occurencies like 'es', 'es-cl' or 'es-CL'
 	window.location.href = "es/index.html";	
 }
-// else if(user_lang == 'fr'){
-// 	window.location.href = "fr/index.html";	
-// }
+else if(user_lang.match(/^fr/)){
+	window.location.href = "fr/index.html";	
+}
+else if(user_lang.match(/^ru/)){
+	window.location.href = "ru/index.html";	
+}
+if(user_lang.match(/^ja/)){
+	window.location.href = "ja/index.html";	
+}
