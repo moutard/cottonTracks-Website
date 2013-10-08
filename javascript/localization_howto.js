e02beb13a79f5ceead208c59dea0f29a42bdea6a
Julihan <julien.deveaux@gmail.com>
2013-10-08 19:21:32 +0200

    adding localization to the howto + remove the redirection to ja, ru in the localization script

--- javascript/localization_howto.js
+++ javascript/localization_howto.js
@@ -1,13 +1,13 @@
 var user_lang = window.navigator.userLanguage || window.navigator.language;
 if(user_lang.match(/^es/)){ //match all occurencies like 'es', 'es-cl' or 'es-CL'
-	window.location.href = "es/index.html";	
+	window.location.href = "es/howto.html";	
 }
 else if(user_lang.match(/^fr/)){
-	window.location.href = "fr/index.html";	
+	window.location.href = "fr/howto.html";	
 }
 // else if(user_lang.match(/^ru/)){
-// 	window.location.href = "ru/index.html";	
+// 	window.location.href = "ru/howto.html";	
 // }
 // if(user_lang.match(/^ja/)){
-// 	window.location.href = "ja/index.html";	
+// 	window.location.href = "ja/howto.html";	
 // }
