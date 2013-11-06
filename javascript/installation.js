1ce0607ca680bfdee8644b26475bad420d57473c
Julihan <julien.deveaux@gmail.com>
2013-11-06 11:52:13 +0100

    fix TNW image url + add the script the remove the install button when the app is already installed

--- javascript/installation.js
+++ javascript/installation.js
@@ -1,15 +1,39 @@
 function ExtInstall() {
+	var paragraphs = document.getElementsByTagName('p');
+	for (i=0; i<paragraphs.length; i++){
+		if (paragraphs[i].hasAttribute("ct-id")){
+			chrome.app.isInstalled = true;
+			break;
+		}
+	}
 	if (chrome.app.isInstalled) 
   	alert("already installed!");
-  else 
+  else{
   	chrome.webstore.install();
 		IsInstalled();
+	}
 }
 function IsInstalled(){
-	if (chrome.app.isInstalled){
+	var appInstalled = false;
+	var paragraphs = document.getElementsByTagName('p');
+	for (i=0; i<paragraphs.length; i++){
+		if (paragraphs[i].hasAttribute("ct-id")){
+			appInstalled = true;
+			break;
+		}
+	}
+	
+	if (appInstalled){
 		var css = document.createElement("style");
 		css.type = "text/css";
 		css.innerHTML = ".download-button { display: none; } .installed-button{ display: inline;}";
 		document.body.appendChild(css);
 	}
+	else
+		window.addEventListener('install', function(){ 
+			var css = document.createElement("style");
+			css.type = "text/css";
+			css.innerHTML = ".download-button { display: none; } .installed-button{ display: inline;}";
+			document.body.appendChild(css);
+		});
 }
